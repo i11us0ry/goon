cb116aa7a39f54f172c166ca15cdccfbda68e767
i11us0ry <34978854+i11us0ry@users.noreply.github.com>
2023-12-25 16:46:28 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 	public.Init(check.ConfigRead(&public.Conf{}))
 	// 初始化输入指令和打印banner
 	public.Flag()
+
 	//处理输入指令
 	check.CheckInput()
 	// 根据mode下发任务
 	check.CheckMode()
 	fmt.Println()
 	eTime := time.Now().Unix()
-	public.Info.Println(fmt.Sprintf("running time：%ds",eTime-sTime))
+	public.Info.Println(fmt.Sprintf("running time：%ds", eTime-sTime))
 }
-
